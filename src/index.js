9998e74052acac5647e303a56f8f56d662c025d3
wonktnodi <wonktnodiq@gmail.com>
2018-06-16 16:51:27 +0800

    add room page with redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import adapter from "webrtc-adapter";
+
+import configureStore from "./stores/configureStore";
+
 import "./index.css";
+
 import Home from "./containers/Home";
+import Room from "./containers/Room";
+
+if (window) {
+  window.adapter = adapter;
+}
+
+const store = configureStore();
 
 function content() {
-  return <Home />;
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Room} />
+          <Route exact path="/rooms" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 ReactDOM.render(content(), document.getElementById("root"));
