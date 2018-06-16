2493799ed00b15936daaf199f1c3a1bbb6065838
wonktnodi <wonktnodiq@gmail.com>
2018-06-16 15:55:22 +0800

    initial version with electron and react

---
+++ public/main.js
@@ -0,0 +1,38 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({ width: 900, height: 680 });
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+
+  app.setAboutPanelOptions({
+    applicationName: "Mook",
+    applicationVersion: "0.0.1"
+  });
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
